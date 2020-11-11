84d155bdb7b4cb3aea65673a6a1d4518c6373c59
Noura Loudani <contact.nouraloudani@gmail.com>
2020-11-11 06:19:07 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -7,7 +7,7 @@ def join_nested_strings(src)
    inner_index = 0
     while inner_index < src[outer_index].length do 
       if src[outer_index][inner_index].class == String
-        phrase += src[outer_index][inner_index]
+        phrase += src[outer_index][inner_index] + " "
      end
      inner_index += 1
     end  
